8305b46dec0f7d0d87084b5cc9021e23495dcbfc
Ira Weinstein <ira@directshen.com>
2014-10-09 15:38:59 -0400

    Initial Commite

---
+++ FM-Library/FMUtilLib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_Library
+{
+    public class FMUtilLib
+    {
+        public FMUtilLib()
+    
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger( typeof( FMUtilLib ) );           
+ 
+            log.Debug( "Sample Debug Log Entry" );
+            log.Info( "Sample Info Log Entry" );
+            log.Warn( "Sample Warning Log ENtry" );
+            log.Error( "Sample Error Log Entry" );
+            log.Fatal( "Sample Fatal Log Entry" );
+            
+            Console.ReadLine();  // so you can read the output
+        }
+      
+        public int addTwo(int a1, int a2)
+        {
+            return (a1 + a2);
+        }  
+    }
+}
---
+++ FMUtilLibrary-UnitTestProject/FMUtilLibTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FM_Library;
+
+namespace FMUtilLibrary_UnitTestProject
+{
+    [TestClass]
+    public class FMUtilLibTest1
+    {
+        [TestMethod]
+        public void testAddTwo()
+        {
+            Assert.AreEqual(2 + 2, new FMUtilLib().addTwo(2, 2));
+        }
+    }
+}
