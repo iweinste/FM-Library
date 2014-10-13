8aa17506f8f725904542b547a27df2fd901d80e0
rduncan@bds.com <rduncan@bds.com>
2014-10-13 11:42:42 -0400

    Added batch command file to enable corporate nuget repo

---
+++ .nuget/prep.bat
@@ -0,0 +1,21 @@
+rem
+rem Prepare user account nuget for use
+rem
+
+rem
+rem Add the 'corporate package source'
+rem
+rem 
+nuget sources add -Name "BDSNugetPublic" -Source "http://majordomo.bds.com:8081/nexus/service/local/nuget/nuget-public/" -username "cdpMM5JW" -password "HuKKDuSIxYB4mSNmjLtF0k/Cu4fthRrc7Hh319BmCYva"
+
+rem
+rem Set the Nuget Release Repo as the default push source
+rem
+nuget config -set DefaultPushSource=http://majordomo.bds.com:8081/nexus/service/local/nuget/nuget-release/
+
+rem
+rem Set the APIKey for the default push source
+rem
+nuget setApiKey 5f2f7b21-6e37-3a83-ae79-7e29243b4356 -Source http://majordomo.bds.com:8081/nexus/service/local/nuget/nuget-release/
+
+
